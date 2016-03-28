0b8ccf7a87665cd9f1457a81826f6b28812bd1da
WebCrew <palareas@gmail.com>
2016-03-28 23:41:16 +0200

    update
    found a little bug

--- widgets/views/messengerframe.php
+++ widgets/views/messengerframe.php
@@ -7,18 +7,9 @@ use humhub\models\Setting;
     <?=Yii::t('MessengerModule.base', '<strong>Messenger</strong>'); ?>
   </div>
   <div class="panel-body">
-  <?php
-  	if (!Setting::Get('messenger')) {
- ?>
- 	Click on "Start App" to run the Messenger, please.
- <?php
-	} else {
-?>
+  
 <script src="https://togetherjs.com/togetherjs-min.js"></script>
 <button onclick="TogetherJS(this); return false;">Start App</button>
-<?php
-	}
-?>
-    
+
   </div>
 </div>
