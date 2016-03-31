a6aab2a0a776275e6fa006a3bdd5f60c3d2b9587
WebCrew <palareas@gmail.com>
2016-03-31 02:19:25 +0200

    Update messengerframe.php

--- widgets/views/messengerframe.php
+++ widgets/views/messengerframe.php
@@ -9,7 +9,7 @@ use humhub\models\Setting;
   <div class="panel-body">
   
 <script src="https://togetherjs.com/togetherjs-min.js"></script>
-<button onclick="TogetherJS(this); return false;">Start App</button>
+<button onclick="TogetherJS(this); return false;">Chat</button>
 
   </div>
 </div>
