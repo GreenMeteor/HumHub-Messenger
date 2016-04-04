fd2c8dc259f0f19185ac70c3f0b5cf678aa64c22
Sarah Tsumayoi <Felli@users.noreply.github.com>
2016-04-03 23:51:51 -0400

    Update index.php

--- views/admin/index.php
+++ views/admin/index.php
@@ -6,16 +6,16 @@ use humhub\models\Setting;
 use humhub\modules\messenger\controllers\AdminController;
 ?>
 <div class="panel panel-default">
-	<div class="panel-heading"><?=Yii::t('MessengerModule.views_messenger_index', '<strong>Messenger</strong>'); ?></div>
+	<div class="panel-heading"><?=Yii::t('MessengerModule.base', '<strong>Messenger</strong>'); ?></div>
 	<div class="panel-body">
 		<?php $form = CActiveForm::begin(['id' => 'messenger-settings-form']); ?>
 			<?=$form->errorSummary($model); ?>
-			<p class="help-block"><?=Yii::t('MessengerModule.views_messenger_index', 'eg:  "999999"'); ?></p>
+			<p class="help-block"><?=Yii::t('MessengerModule.base', 'eg:  "999999"'); ?></p>
 			<div class="form-group">
 				<?=$form->labelEx($model, 'sort'); ?>
 				<?=$form->textField($model, 'sort', ['class' => 'form-control', 'readonly' => Setting::IsFixed('sort', 'messenger')]); ?>
 			</div>
-			<p class="help-block"><?php echo Yii::t('MessengerModule.views_messenger_index', 'Widget Positioning') ?></p>
+			<p class="help-block"><?php echo Yii::t('MessengerModule.base', 'Widget Positioning') ?></p>
 			<?php echo Html::(Yii::t('submitButton(Yii:t('messengerModule.views_messenger_index', 'Save'), ['class' => 'btn-primary']); ?>
 			<?=\humhub\widgets\DataSaved::widget(); ?>
 		<?php CActiveForm::end(); ?>
