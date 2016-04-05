bd5446ab34bf0a1a60627bdefebab19baa890fbd
Sarah Tsumayoi <Felli@users.noreply.github.com>
2016-04-05 03:43:13 -0400

    Update messengerframe.php

--- widgets/views/messengerframe.php
+++ widgets/views/messengerframe.php
@@ -8,8 +8,8 @@ use humhub\models\Setting;
   </div>
   <div class="panel-body">
   
-<?php $this->registerJsFile( $this->getBaseUrl().'/js/togetherjs.js', ['position'=>\yii\web\View::Sidebar]); ?>
-<?php $this->registerJsFile( $this->getBaseUrl().'/js/togetherjs-min.js', ['position'=>\yii\web\View::Sidebar]); ?>
+<?php $this->registerJsFile( $this->getUrl().'/js/togetherjs.js', ['position'=>\yii\web\View::Sidebar]); ?>
+<?php $this->registerJsFile( $this->getUrl().'/js/togetherjs-min.js', ['position'=>\yii\web\View::Sidebar]); ?>
 <button onclick="TogetherJS(this); return false;">Chat</button>
 
   </div>
